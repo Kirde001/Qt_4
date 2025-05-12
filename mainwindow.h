107748ef63d69fbde37fe6f170fd0ca65e111655
Kirill Poliakov <145561178+Kirde001@users.noreply.github.com>
2025-05-12 17:08:28 +0300

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -1,9 +1,11 @@
-#ifndef MAINWINDOW_H // Исправлен инклуд-гард
+#ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
 #include <QMainWindow>
 #include <QSlider>
 #include <QVBoxLayout>
+#include <QLineEdit> // Added for input field
+#include <QLabel>    // Added for input field label
 #include "spiralprogressindicator.h"
 
 class MainWindow : public QMainWindow
@@ -14,9 +16,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    // Слот для обработки завершения редактирования поля ввода
+    void onInputEditingFinished();
+    // Слот для обновления поля ввода при изменении значения прогресса
+    void onProgressValueChanged(int value);
+
 private:
     SpiralProgressIndicator *progressIndicator;
     QSlider *slider;
+    QLineEdit *valueInput; // Поле ввода для значения
+    QLabel *inputLabel;    // Метка для поля ввода
 };
 
-#endif // MAINWINDOW_H // Исправлен инклуд-гард
+#endif // MAINWINDOW_H
